68ed9d80d8d65e9e953a013e716027d1f0a22b82
mruizcas04 <mruizcas04@gmail.com>
2025-06-16 19:19:41 +0200

    correcciones

--- src/procesador/AnalizadorLexico.java
+++ src/procesador/AnalizadorLexico.java
@@ -469,7 +469,14 @@ public class AnalizadorLexico {
                 if (c == '/') {
                     state = 0;
                     return 'A';
-                } else {
+
+                }
+
+                else if (c == '*') {
+                    return 'A';
+                }
+
+                else {
                     state = 7;
                     return 'A';
                 }
